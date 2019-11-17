c60d4fe613f07a4fc795f9e466387bc72373719a
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-17 03:17:05 -0500

    added slide 2

--- presentation/index.js
+++ presentation/index.js
@@ -7,7 +7,7 @@ const theme = createTheme(
   {
     primary: '#2D55B3',
     secondary: '#80A6FF',
-    tertiary: '#6565FF'
+    tertiary: '#6694FF'
   },
   {
     primary: 'Montserrat',
@@ -18,9 +18,20 @@ export default class Presentation extends Component {
   render() {
     return (
       <Deck theme={theme}>
+        {/* Intro */}
         <Slide>
-          <Heading margin="150px" size={5} textColor="secondary" bold>Dapps with Ethereum & Web3.js</Heading>
+          <Heading margin="150px" size={5} textColor="secondary" bold>DApps with Ethereum & Web3.js</Heading>
           <Text textColor="secondary">Pradhumna Pancholi</Text>
+        </Slide>
+
+        {/* What are Dapps */}
+        <Slide>
+          <Heading margin="0 0 50px 0" size={5} textColor="secondary" bold>What are DApps</Heading>
+          <Text textColor="secondary">
+            Dapps are applications that run on a blockchain network. Blockchains are known for their "decentralized" nature. That's where the 'D' comes from.
+            A DApp could be written for any platform. So, in the most basic terms, it's like using a blockchain network a database instead of a traditional database.
+            After cryptocurrencies, this is most pouplar use case of blockchain.
+          </Text>
         </Slide>
       </Deck>
     );
