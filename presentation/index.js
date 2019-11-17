24058cda86ebcfde6754595adfadfa1a4ba43278
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-17 02:54:26 -0500

    added intro slide and custom theme

--- presentation/index.js
+++ presentation/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { Deck, Slide } from 'spectacle';
+import { Deck, Heading, Slide, Text } from 'spectacle';
+import createTheme from 'spectacle/lib/themes/default';
 
+// for theme//
+const theme = createTheme(
+  {
+    primary: '#2D55B3',
+    secondary: '#80A6FF',
+    tertiary: '#6565FF'
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
+  }
+);
 export default class Presentation extends Component {
   render() {
     return (
-      <Deck>
-        <Slide>Spectable 1</Slide>
+      <Deck theme={theme}>
+        <Slide>
+          <Heading margin="150px" size={5} textColor="secondary" bold>Dapps with Ethereum & Web3.js</Heading>
+          <Text textColor="secondary">Pradhumna Pancholi</Text>
+        </Slide>
       </Deck>
     );
   }
